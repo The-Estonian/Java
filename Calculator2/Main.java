a512c84bfd8fb0dec98a8649ccaa589a5ad9de0c
The-Estonian <Zaar2213@gmail.com>
2024-09-22 15:40:36 +0300

    added loop logic to calculator2

--- Calculator2/Main.java
+++ Calculator2/Main.java
@@ -7,21 +7,38 @@ import Calculator2.calculator.Calculator;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Calculator calc = new Calculator();
 
         System.out.println("Please enter the first number: ");
-        int num1 = scan.nextInt();
+        calc.setNum1(scan.nextInt());
         scan.nextLine(); // aparently consumes the leftover space
 
         System.out.println("Please enter the operator: ");
         String operator = scan.nextLine();
 
         System.out.println("Please enter the second number: ");
-        int num2 = scan.nextInt();
-        scan.close();
-
-        Calculator calc = new Calculator(num1, num2);
+        calc.setNum2(scan.nextInt());
+        scan.nextLine();
 
         calc.calculate(operator);
 
+        boolean running = true;
+
+        while (running) {
+            if ("x".equals(operator)) {
+                running = false;
+            } else {
+                System.out.println("Please enter the next operator or exit with 'x': ");
+                operator = scan.nextLine();
+
+                System.out.println("Please enter another number: ");
+                calc.setNum2(scan.nextInt());
+                scan.nextLine();
+
+                calc.calculate(operator);
+            }
+        }
+
+        scan.close();
     }
 }
--- Calculator2/calculator/Calculator.java
+++ Calculator2/calculator/Calculator.java
@@ -4,8 +4,11 @@ public class Calculator {
     private int num1;
     private int num2;
 
-    public Calculator(int num1, int num2) {
+    public void setNum1(int num1) {
         this.num1 = num1;
+    }
+
+    public void setNum2(int num2) {
         this.num2 = num2;
     }
 
