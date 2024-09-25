3e767120708093269c788c7a7aa73bbb5d347185
The-Estonian <Zaar2213@gmail.com>
2024-09-25 19:51:14 +0300

    finished filemanager

---
+++ FileManager/FileManager.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.File;
+
+public class FileManager {
+    public static void createFile(String fileName, String content) throws IOException {
+
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            writer.write(content);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String getContentFile(String fileName) throws IOException {
+        StringBuilder fullString = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                fullString.append(line).append("\n");
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fullString.toString();
+    }
+
+    public static void deleteFile(String fileName) {
+        File file = new File(fileName);
+        file.delete();
+    }
+}
