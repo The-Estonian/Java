92f34a450235b1e0b64496ee38203d0f8f43c6be
The-Estonian <Zaar2213@gmail.com>
2024-09-25 17:27:19 +0300

    finished catinfile

--- CatInFile/CatInFile.java
+++ CatInFile/CatInFile.java
@@ -1,8 +1,20 @@
- 
 import java.io.*;
 
 public class CatInFile {
     public static void cat(String[] args) throws IOException {
-        
+        if (args.length != 1) {
+            System.err.println("Usage: java CatInFile <output file>");
+            return;
+        }
+
+        String outputFileName = args[0];
+        try (InputStream in = System.in;
+                OutputStream out = new FileOutputStream(outputFileName)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        }
     }
 }
