4b64c3fa14dca57b275fe712f4dc16c77ef08612
The-Estonian <Zaar2213@gmail.com>
2024-09-25 22:20:48 +0300

    finished stargetters

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,52 @@
+public class CelestialObject {
+    private double x;
+    private double y;
+    private double z;
+    private String name;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+}
