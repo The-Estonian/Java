bcc46c10dde40d1fc797e08516b8b0aca67dd33e
The-Estonian <Zaar2213@gmail.com>
2024-09-25 10:31:56 +0300

    finished cleanextract

--- cleanextract/CleanExtract.java
+++ cleanextract/CleanExtract.java
@@ -1,18 +1,28 @@
-package cleanextract;
-
 public class CleanExtract {
     public static String extract(String s) {
-        String returnString = "";
-        String[] strArr = s.split("\\|");
-        for (int i = 0; i < strArr.length; i++) {
-            int stringFirstDot = strArr[i].indexOf('.');
-            int stringSecondDot = strArr[i].lastIndexOf(".");
-            if (stringFirstDot != -1 && stringSecondDot != -1 && stringFirstDot != stringSecondDot) {
-                returnString += strArr[i].substring(stringFirstDot1   stringSecondDot).trim();
+        StringBuilder returnString = new StringBuilder();
+        String[] splitStrings = s.split("\\|");
+        for (int i = 0; i < splitStrings.length; i++) {
+            int startDot = splitStrings[i].indexOf('.');
+            int endDot = splitStrings[i].lastIndexOf('.');
+            if (startDot != -1 && endDot != -1 && startDot < endDot) {
+                String extracted = splitStrings[i].substring(startDot + 1, endDot).trim();
+                if (!extracted.isEmpty()) {
+                    returnString.append(extracted).append(" ");
+                }
+            } else if (startDot != -1) {
+                String extracted = splitStrings[i].substring(startDot + 1).trim();
+                if (!extracted.isEmpty()) {
+                    returnString.append(extracted).append(" ");
+                }
             } else {
-                returnString += strArr[i].trim();
+                String extracted = splitStrings[i].trim();
+                if (!extracted.isEmpty()) {
+                    returnString.append(extracted).append(" ");
+                }
             }
         }
-        return returnString;
+
+        return returnString.toString().trim();
     }
 }
