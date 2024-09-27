a5872c3b6d9b603e33a30f8f709ddc37a9f3c807
The-Estonian <Zaar2213@gmail.com>
2024-09-27 12:53:07 +0300

    finished starinheritance

---
+++ StarInheritance/CelestialObject.java
@@ -0,0 +1,90 @@
+
+import java.util.Objects;
+
+public class CelestialObject {
+    public static final double KM_IN_ONE_AU = 150_000_000.0;
+
+    private double x;
+    private double y;
+    private double z;
+    private String name;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public static double getDistanceBetween(CelestialObject obj1, CelestialObject obj2) {
+        double deltaX = obj2.getX() - obj1.getX();
+        double deltaY = obj2.getY() - obj1.getY();
+        double deltaZ = obj2.getZ() - obj1.getZ();
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject obj1, CelestialObject obj2) {
+        double distanceInAu = getDistanceBetween(obj1, obj2);
+        return distanceInAu * KM_IN_ONE_AU;
+    }
+
+    public String toString() {
+        return String.format("%s is positioned at (%.3f, %.3f, %.3f)", this.name, this.x, this.y, this.z);
+    }
+
+    public Boolean equals(CelestialObject obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        return Double.compare(obj.x, x) == 0 &&
+                Double.compare(obj.y, y) == 0 &&
+                Double.compare(obj.z, z) == 0 &&
+                Objects.equals(name, obj.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, x, y, z);
+    }
+}
+
