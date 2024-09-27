cc7c85fd6fec61bf286b001c5d212f15caa587b3
The-Estonian <Zaar2213@gmail.com>
2024-09-27 22:09:23 +0300

    finished starplanet

---
+++ StarPlanet/Star.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public Star() {
+        this.magnitude = 0.0;
+    }
+
+    public Star(String name, double x, double y, double z, double magnitude) {
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+    }
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public double getMagnitude() {
+        return this.magnitude;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        if (!super.equals(obj))
+            return false;
+
+        Star star = (Star) obj;
+        return Double.compare(this.magnitude, star.magnitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), magnitude);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s shines at the %.3f magnitude", getName(), magnitude);
+    }
+}
