da008be30efb5ef3fcfed2ed579e79bee59bd6f0
The-Estonian <Zaar2213@gmail.com>
2024-09-28 11:53:05 +0300

    finished adventuresorcerer

---
+++ AdventureSorcerer/Monster.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster extends Character {
+
+    public Monster(String name, int maxHealth) {
+        super(name, maxHealth);
+
+    }
+
+    @Override
+    public String toString() {
+        return getCurrentHealth() == 0 ? String.format("%s is a monster and is dead",
+                getName())
+                : String.format("%s is a monster with %d HP", getName(), getCurrentHealth());
+    }
+
+}
