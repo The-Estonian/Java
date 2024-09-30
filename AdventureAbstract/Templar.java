32ae6092550889ee4751b13f1a17412df838f201
The-Estonian <Zaar2213@gmail.com>
2024-09-30 13:36:25 +0300

    finished adventureabstract

---
+++ AdventureAbstract/Templar.java
@@ -0,0 +1,60 @@
+public class Templar extends Character implements Healer, Tank {
+    private final int healCapacity;
+    private final int shield;
+
+    public Templar(String name, int maxHealth, int heal, int shield) {
+        super(name, maxHealth);
+        this.healCapacity = heal;
+        this.shield = shield;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return this.healCapacity;
+    }
+
+    // Heal
+    @Override
+    public void heal(Character character) {
+        int newHealth = character.getCurrentHealth() + this.healCapacity;
+        character.setCurrentHealth(Math.min(newHealth, character.getMaxHealth()));
+    }
+
+    // Get Shield
+    @Override
+    public int getShield() {
+        return this.shield;
+    }
+
+    // Take damage
+    @Override
+    public void takeDamage(int damage) {
+        damage = damage - this.shield;
+        if (damage > 0) {
+            if (this.getCurrentHealth() - damage < 0) {
+                this.setCurrentHealth(0);
+            } else {
+                this.setCurrentHealth(this.getCurrentHealth() - damage);
+            }
+        }
+    }
+
+    // Attack
+    @Override
+    public void attack(Character character) {
+        this.heal(this);
+        character.takeDamage(6);
+    }
+
+    @Override
+    public String toString() {
+        return this.getCurrentHealth() == 0
+                ? String.format(
+                        "%s has been beaten, even with its %d shield. So bad, it could heal %d HP.",
+                        this.getName(), this.shield,
+                        this.healCapacity)
+                : String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d.",
+                        this.getName(),
+                        this.getCurrentHealth(), this.getHealCapacity(), this.shield);
+    }
+}
