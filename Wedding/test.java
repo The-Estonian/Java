3f3d9dc9f8895a1c3f0f39440efc7a19c6c91d1a
jaanus <zaar2213@gmail.com>
2024-10-02 23:51:12 +0300

    finished wedding

---
+++ Wedding/test.java
@@ -0,0 +1,7 @@
+import java.util.Set;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(Wedding.createCouple(Set.of("Pikachu", "Dracaufeu", "Tortank"), Set.of("Legolas", "Aragorn", "Gimli")));
+    }
+}
