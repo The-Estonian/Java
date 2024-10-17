d2f67a8a9cf387c5db61c6e7a33564ce33b07827
jaanus <zaar2213@gmail.com>
2024-10-17 22:12:25 +0300

    finished singleton

---
+++ Singleton/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        // Get the singleton instance of Excalibur and print its name
+        System.out.println(Excalibur.getInstance().getName());
+    }
+}
